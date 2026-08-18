821090585224fa0efb70ebd063b6e829563a1cea
reiiwaguri <iryuukazaya1998@gmail.com>
2026-08-18 16:54:33 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// Ganti "/sisi-otaku/" dengan "/NAMA-REPO-KAMU/" jika nama repo GitHub kamu berbeda.
-// Jika deploy ke Vercel/Netlify (bukan GitHub Pages), ganti base menjadi "/".
-export default defineConfig({
-  plugins: [react()],
-  base: "/sisi-otaku/",
-});
